9e2e91d8e004052f08634e6be0323db8c8a70a43
objshka <yazkiri4@gmail.com>
2023-03-17 20:31:43 +0300

    add Person(1hw)

---
+++ Person/Person/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Person
+{
+    public class Human
+    {
+        private string _name;
+        private int _age;
+
+        public Human(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Имя: " + _name + "\nВозраст: " + _age);
+        }
+    }
+}
---
+++ Person/Person/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Person
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Human human = new Human("Ivan", 25);
+            
+            human.ShowInfo();
+        }
+    }
+}
